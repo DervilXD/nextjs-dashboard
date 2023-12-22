63022ec3f3c2e52f4dd6162737f1ad1b435f3c06
Leonardo <dervil.dev@gmail.com>
2023-12-22 15:35:26 +0000

    add routing pages, layout and link navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
-        <h1>Este Titulo</h1>
-      <div style={{ border: '1px solid #000'}}>
-          {children}
-        </div>
-        </body>
+        {children}
+      </body>
     </html>
   );
 }
